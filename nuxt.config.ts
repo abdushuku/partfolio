9e707b3e6e354fb7c45fed4c28478d332a744bcd
Abdushukur <abduhakimovabdushukur@gmail.com>
2024-09-16 18:30:12 +0500

    change nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  colorMode: {
-    preference: 'light'
-  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxt/icon', '@nuxt/ui'],
